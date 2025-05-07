15daabdeee056afc55955b67bf9e81386f120a45
Eduard8813 <143306086+Eduard8813@users.noreply.github.com>
2025-05-07 14:17:46 -0600

    base

--- control.sql
+++ control.sql
@@ -1,1 +1,12 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS prueba;
 
+-- Usar la base de datos creada
+USE prueba;
+
+-- Crear la tabla users
+CREATE TABLE sensors (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  temperature FLOAT,
+  humidity FLOAT
+);
--- database.php
+++ database.php
@@ -1,1 +1,15 @@
+<?php
+// Configuration
+$db_host = '127.0.0.1';
+$db_username = 'root';
+$db_password = '';
+$db_name = 'prueba';
 
+// Create connection
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
